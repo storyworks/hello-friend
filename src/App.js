fb44a35fc7265d26cdd2b93890ac0faf74c11e3d
Ziwei Liu <ziwei.liu.23@gmail.com>
2019-12-07 18:51:20 +1300

    Make more sensitive

--- src/App.js
+++ src/App.js
@@ -4,6 +4,26 @@ import logo from "./logo.svg";
 import { questions } from "./questions";
 import "./App.css";
 
+const Direction = {
+  DOWN: Symbol("DOWN"),
+  LEFT: Symbol("LEFT"),
+  RIGHT: Symbol("RIGHT"),
+  UP: Symbol("UP")
+};
+
+const config = {
+  // allowedDirections: [Direction.DOWN, Direction.LEFT, Direction.RIGHT],
+  throwOutConfidence: (xOffset, yOffset, element) => {
+    // const xConfidence = Math.min(Math.abs(xOffset) / element.offsetWidth, 1);
+    // const yConfidence = Math.min(Math.abs(yOffset) / element.offsetHeight, 1);
+
+    // return Math.max(xConfidence, yConfidence);
+    return 1;
+  }
+
+  //rotation: 50
+};
+
 class App extends Component {
   render() {
     return (
@@ -12,16 +32,18 @@ class App extends Component {
         <div id="viewport">
           <Swing
             className="stack"
-            tagName="div"
+            // tagName="div"
             setStack={stack => this.setState({ stack: stack })}
+            config={config}
             ref="stack"
-            throwout={e => console.log("throwout", e)}
+            throwout={e => console.log("throwout stack", e)}
           >
             {questions.map(question => (
               <div
                 className="card"
                 ref={question.id}
-                throwout={e => console.log("card throwout", e)}
+                key={question.id}
+                throwout={e => console.log(e)}
               >
                 {question.message}
               </div>
