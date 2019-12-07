fb5eb6453b67878e2074589054961ede09e55d73
Ziwei Liu <ziwei.liu.23@gmail.com>
2019-12-08 10:27:33 +1300

    Working react-spring implementation

--- src/App.js
+++ src/App.js
@@ -1,55 +1,15 @@
 import React, { Component } from "react";
-import Swing from "react-swing";
+import { Deck } from "./Deck";
 import logo from "./logo.svg";
-import { questions } from "./questions";
+
 import "./App.css";
-
-const Direction = {
-  DOWN: Symbol("DOWN"),
-  LEFT: Symbol("LEFT"),
-  RIGHT: Symbol("RIGHT"),
-  UP: Symbol("UP")
-};
-
-const config = {
-  // allowedDirections: [Direction.DOWN, Direction.LEFT, Direction.RIGHT],
-  throwOutConfidence: (xOffset, yOffset, element) => {
-    // const xConfidence = Math.min(Math.abs(xOffset) / element.offsetWidth, 1);
-    // const yConfidence = Math.min(Math.abs(yOffset) / element.offsetHeight, 1);
-
-    // return Math.max(xConfidence, yConfidence);
-    return 1;
-  }
-
-  //rotation: 50
-};
 
 class App extends Component {
   render() {
     return (
       <>
         <img src={logo} className="logo" alt="logo" />
-        <div id="viewport">
-          <Swing
-            className="stack"
-            // tagName="div"
-            setStack={stack => this.setState({ stack: stack })}
-            config={config}
-            ref="stack"
-            throwout={e => console.log("throwout stack", e)}
-          >
-            {questions.map(question => (
-              <div
-                className="card"
-                ref={question.id}
-                key={question.id}
-                throwout={e => console.log(e)}
-              >
-                {question.message}
-              </div>
-            ))}
-          </Swing>
-        </div>
+        <Deck />
       </>
     );
   }
--- src/questions.js
+++ src/questions.js
@@ -7,5 +7,72 @@ export const questions = [
   {
     id: "card2",
     message: "What's your favourite piece of clothing you own / owned?"
+  },
+  {
+    id: "card3",
+    message: "What hobby would you get into if time and money weren't an issue?"
+  },
+  {
+    id: "card4",
+    message: "What would your perfect room look like?"
+  },
+  {
+    id: "card5",
+    message: "How often do you play sports?"
+  },
+  {
+    id: "card6",
+    message: "What fictional place would you most like to go?"
+  },
+  {
+    id: "card7",
+    message: "What job would you be terrible at?"
+  },
+  {
+    id: "card8",
+    message: "When was the last time you climbed a tree?"
+  },
+  {
+    id: "card9",
+    message:
+      "If you could turn any activity into an Olympic sport, what would you have a good chance at winning a medal for?"
+  },
+  {
+    id: "card10",
+    message: "What is the most annoying habit that other people have?"
+  },
+  {
+    id: "card11",
+    message: "What job do you think you'd be really good at?"
+  },
+  {
+    id: "card12",
+    message: "What skill would you like to master?"
+  },
+  {
+    id: "card13",
+    message: "What would be the most amazing adventure to go on?"
+  },
+  {
+    id: "card14",
+    message:
+      "If you had unlimited funds to build a house that you would live in for the rest of your life, what would the finished house be like?"
+  },
+  {
+    id: "card15",
+    message: "What's your favourite drink?"
+  },
+  {
+    id: "card16",
+    message: "What state or country do you never want to go back to?"
+  },
+  {
+    id: "card17",
+    message: "What songs have you completely memorized?"
+  },
+  {
+    id: "card14",
+    message:
+      "If you had unlimited funds to build a house that you would live in for the rest of your life, what would the finished house be like?"
   }
 ];
