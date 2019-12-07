b7e29882c29772eaa8ade3b53df1e7f80a622ded
Ziwei Liu <ziwei.liu.23@gmail.com>
2019-12-07 18:05:09 +1300

    Make responsive

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
 import React, { Component } from "react";
 import Swing from "react-swing";
+import logo from "./logo.svg";
 import { questions } from "./questions";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div id="viewport">
-        <Swing
-          className="stack"
-          tagName="div"
-          setStack={stack => this.setState({ stack: stack })}
-          ref="stack"
-          throwout={e => console.log("throwout", e)}
-        >
-          {questions.map(question => (
-            <div
-              className="card"
-              ref={question.id}
-              throwout={e => console.log("card throwout", e)}
-            >
-              {question.message}
-            </div>
-          ))}
-        </Swing>
-      </div>
+      <>
+        <img src={logo} className="logo" alt="logo" />
+        <div id="viewport">
+          <Swing
+            className="stack"
+            tagName="div"
+            setStack={stack => this.setState({ stack: stack })}
+            ref="stack"
+            throwout={e => console.log("throwout", e)}
+          >
+            {questions.map(question => (
+              <div
+                className="card"
+                ref={question.id}
+                throwout={e => console.log("card throwout", e)}
+              >
+                {question.message}
+              </div>
+            ))}
+          </Swing>
+        </div>
+      </>
     );
   }
 }
